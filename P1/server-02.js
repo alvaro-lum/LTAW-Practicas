49dfe960dd20fe1136386e1fbcce64f0289440c8
alvarolr <a.lumbreras.2019@alumnos.urjc.es>
2024-02-12 12:36:49 +0100

    cambios en server 2

--- P1/server-02.js
+++ P1/server-02.js
@@ -4,17 +4,14 @@ const http = require('http');
 //-- Definir el puerto a utilizar
 const PUERTO = 8080;
 
-//-- Función de retrollamada de petición recibida
-function atender(req, res) {
-
-    //-- Indicamos que se ha recibido una petición
-    console.log("Petición recibida!");
-}
-
-//-- Crear el servidor. Se pasa como argumento la 
-//-- función de retrollamada. La función createServer()
-//-- la conecta con el evento 'request'
-const server = http.createServer(atender);
+//-- Crear el servidor. La función de retrollamada de
+//-- atención a las peticiones se define detnro de los
+//-- argumentos
+const server = http.createServer((req, res) => {
+    
+  //-- Indicamos que se ha recibido una petición
+  console.log("Petición recibida!");
+});
 
 //-- Activar el servidor: ¡Que empiece la fiesta!
 server.listen(PUERTO);
