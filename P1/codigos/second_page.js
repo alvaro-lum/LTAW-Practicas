e36b4ef555d9d04d9fc0676a4d41da6d91fea303
alvarolr <a.lumbreras.2019@alumnos.urjc.es>
2024-02-26 12:07:20 +0100

    funcion para poder cambiar de imagenes

--- P1/codigos/second_page.js
+++ P1/codigos/second_page.js
@@ -13,6 +13,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
         if(index < 0){ index = fileListLen -1}
         index = Math.abs(index % fileListLen)
 
-        
+        var ppt= document.getElementById("ppt" + String(index));
+        ppt.style.textShadow = "0.75px 0.75px 0 #1b1b1b1, -0.75px -0.75px 0 #1b1b1b,  0.75px -0.75px 0 #1b1b1b,   -0.75px 0.75px 0 #1b1b1b"
+
+        file[file.length - 1] = String(index) + ".png"
+        img_url[img_url.length - 1] = file.join("_")
+
+        image.src = img_url.join("/")
+        file = img_url[img_url.length - 1].split("_")
+        index = file[file.length - 1].split(".")[0]
     }
+
+    var next = document.getElementById("")
 })
