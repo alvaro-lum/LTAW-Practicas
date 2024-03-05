aa6674610c1453fd16c552c5e87481e210445170
alvaro-lum <123627973+alvaro-lum@users.noreply.github.com>
2024-03-05 14:27:38 +0100

    cambios fotos

--- P1/codigos/second_page.js
+++ P1/codigos/second_page.js
@@ -1,29 +1,27 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     function pasFoto(step) {
-
         // Tomar fotos
-        image = document.getElementById("pic_producto");
-        fileListLen = 3;
-        img_url =image.src.split("/");
-        fie = img_url[img_url.length - 1].split("_")
-        index = parseInt(file[file.length - 1].split(".")[0])
+        var image = document.getElementById("pic_producto");
+        var fileListLen = 3;
+        var img_url = image.src.split("/");
+        var file = img_url[img_url.length - 1].split("_");
+        var index = parseInt(file[file.length - 1].split(".")[0]);
 
         //Actualizarla
-        index += parseInt(step)
-        if(index < 0){ index = fileListLen -1}
-        index = Math.abs(index % fileListLen)
+        index += parseInt(step);
+        if (index < 0) { index = fileListLen - 1; }
+        index = Math.abs(index % fileListLen);
 
-        var ppt= document.getElementById("ppt" + String(index));
-        ppt.style.textShadow = "0.75px 0.75px 0 #1b1b1b1, -0.75px -0.75px 0 #1b1b1b,  0.75px -0.75px 0 #1b1b1b,   -0.75px 0.75px 0 #1b1b1b"
+        var ppt = document.getElementById("ppt" + String(index));
+        ppt.style.textShadow = "0.75px 0.75px 0 #1b1b1b1, -0.75px -0.75px 0 #1b1b1b,  0.75px -0.75px 0 #1b1b1b,   -0.75px 0.75px 0 #1b1b1b";
 
-        file[file.length - 1] = String(index) + ".png"
-        img_url[img_url.length - 1] = file.join("_")
+        file[file.length - 1] = String(index) + ".png";
+        img_url[img_url.length - 1] = file.join("_");
 
-        image.src = img_url.join("/")
-        file = img_url[img_url.length - 1].split("_")
-        index = file[file.length - 1].split(".")[0]
+        image.src = img_url.join("/");
+        file = img_url[img_url.length - 1].split("_");
+        index = file[file.length - 1].split(".")[0];
     }
-
 
     var prev = document.getElementById("fotoBotonPrev");
     var next = document.getElementById("fotoBotonNext");
@@ -33,15 +31,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
     var ppt2 = document.getElementById("ppt2");
 
     prev.onclick = function() {
-        ppt0.style.textShadow = "none"
-        ppt1.style.textShadow = "none"
-        ppt2.style.textShadow = "none"
-        pasFoto(-1)
+        ppt0.style.textShadow = "none";
+        ppt1.style.textShadow = "none";
+        ppt2.style.textShadow = "none";
+        pasFoto(-1);
     };
     next.onclick = function() {
-        ppt0.style.textShadow = "none"
-        ppt1.style.textShadow = "none"
-        ppt2.style.textShadow = "none"
-        pasFoto(1)
+        ppt0.style.textShadow = "none";
+        ppt1.style.textShadow = "none";
+        ppt2.style.textShadow = "none";
+        pasFoto(1);
     };
 });
+
