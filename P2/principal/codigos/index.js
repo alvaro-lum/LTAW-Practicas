9aa355099d0e38ee1c06755004c136c121e078ff
alvaro-lum <123627973+alvaro-lum@users.noreply.github.com>
2024-03-28 14:08:49 +0100

    primer tramo index.js

---
+++ P2/principal/codigos/index.js
@@ -0,0 +1,66 @@
+function showOnScroll() {
+    deleteAnim = document.getElementsByClassName("opacityOnLoad3")
+    for (var i = 0; i < deleteAnim.length; i++) {
+        deleteAnim[i].classList.remove('opacityOnLoad3');
+    }
+    var elementsToShow = document.getElementsByClassName("fade");
+    for (var i = 0; i < elementsToShow.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = elementsToShow[i].getBoundingClientRect().top;
+        elementsToShow[i].style.opacity = 0.3 + 1.95*(windowHeight - elementTop)/windowHeight
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function(event){
+
+    window.addEventListener('scroll', showOnScroll);
+    names = document.getElementsByClassName("reviewName");
+    coments = document.getElementsByClassName("reviewText");
+    images = document.getElementsByClassName("reviewUserImage");
+
+    var nextRev = document.getElementById("reviewButtonNext");
+    var prevRev = document.getElementById("reviewButtonPrev");
+
+    index = 0
+    function showReview(){
+        names[0].innerHTML = reviews[ Math.abs(index % reviews.length)].name
+        coments[0].innerHTML = reviews[ Math.abs(index % reviews.length)].coment
+        images[0].innerHTML = reviews[Math.abs(index % reviews.length)].image
+    }
+    auto = 0
+
+    setInterval(function(){
+        auto += 1
+        if(auto >= 5){
+            index += 1
+            showReview()
+            auto=0
+        }
+    }, 1000);
+
+    nextRev.onclick = function( ){
+        index += 1
+        showReview()
+        auto=0
+    };
+    prevRev.onclick = function() {
+        index += 1
+        showReview()
+        auto=0
+    };
+
+    reviews = []
+
+    var m = new XMLHttpRequest();
+    m.open("GET", "/getReviews", true);
+    m.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    m.onreadystatechange = function() {
+        if (m.readyState==4 && m.status == 200) {
+            reviews = JSON.parse(m.responseText)
+            showReview()
+        }
+    };
+    m.send();
+    });
+
+    
